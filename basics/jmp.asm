94f3e11eea6a1fe487178fc879d7692fc6c5be35
frozen-beak <77727680+frozen-beak@users.noreply.github.com>
2024-12-22 16:33:17 +0000

    fixed spacing

--- basics/jmp.asm
+++ basics/jmp.asm
@@ -5,7 +5,7 @@ _start:
 	mov ebx, 42     ; exit status
 	mov eax, 1      ; exit sys call
 	jmp skip        ; jump to skip section
-	mov ebx, 10	; update exit status (ignored)
+	mov ebx, 10		; update exit status (ignored)
 skip:
 	int 0x80
 
